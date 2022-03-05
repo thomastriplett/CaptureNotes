57735df4dd6184da93a9ea2f1b99f1d84dea15a5
Thomas Triplett <ttriplett@wisc.edu>
2022-03-05 14:22:18 -0600

    Added saveNote function to ImageActivity, integrating it with NotesActivity

--- app/src/main/java/com/thomastriplett/capturenotes/ImageActivity.java
+++ app/src/main/java/com/thomastriplett/capturenotes/ImageActivity.java
@@ -1,11 +1,15 @@
 package com.thomastriplett.capturenotes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +19,7 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -30,6 +35,9 @@ public class ImageActivity extends AppCompatActivity {
 
     private static final int RC_OCR_CAPTURE = 9003;
     private final String TAG = "In ImageActivity";
+
+    DBHelper dbHelper;
+    SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +64,7 @@ public class ImageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 writeToFile(recordText.getText().toString());
+                saveNote(recordText.getText().toString());
             }
         });
     }
@@ -115,11 +124,29 @@ public class ImageActivity extends AppCompatActivity {
             fos.write(System.getProperty("line.separator").getBytes());
             fos.write(recording.getBytes());
             fos.close();
-            Toast.makeText(ImageActivity.this, "Note Saved", Toast.LENGTH_SHORT).show();
+            Toast.makeText(ImageActivity.this, "Note Saved to File", Toast.LENGTH_SHORT).show();
         }
         catch (IOException e) {
             Toast.makeText(ImageActivity.this, "Note Not Saved, Try Changing the Title", Toast.LENGTH_SHORT).show();
             Log.e("Exception", "File write failed: " + e.toString());
         }
+    }
+
+    private void saveNote(String recording) {
+
+        Context context = getApplicationContext();
+        sqLiteDatabase = context.openOrCreateDatabase("notes",
+                Context.MODE_PRIVATE,null);
+        dbHelper = new DBHelper(sqLiteDatabase);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","");
+
+        String title = noteTitle.getText().toString();
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date());
+
+        dbHelper.saveNotes(username, title, recording, date);
+        Toast.makeText(ImageActivity.this, "Note Saved to DB", Toast.LENGTH_SHORT).show();
     }
 }
